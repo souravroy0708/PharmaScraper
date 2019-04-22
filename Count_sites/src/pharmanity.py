fa0ba19d5459b9eb23c2405f57cb212ca544ccf3
Anuj Chatterjee <anujc@finarbconsulting.com>
2019-04-22 16:37:53 +0530

    Update to pharmantity.py

--- Count_sites/src/pharmanity.py
+++ Count_sites/src/pharmanity.py
@@ -1,5 +1,5 @@
 import requests
-import urllib
+import urllib2
 import pymongo
 import logging
 import threading
@@ -19,64 +19,64 @@ except:
 
 urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', soup.text)[0].split(";")
 urls =[i.split("=")[1].replace("'","") if "Region" in i else i.replace("'","") for i in urls]
-sites = []
+locsites = {}
 #loc = {}
 # find out zip code of url
-locat = urls[i].replace(url,"")
-
-for url in urls:
-    if ('www.' in url):
-        page = urllib.request.urlopen(url).read()
+for i in range(0, len(urls)):
+    if ('www' in str(urls[i])):
+        sites = []
+        url = str(urls[i])
+        locat = url.split("/")[len(url.split("/"))-1]
+        print(locat)
+        page = urllib2.urlopen(url)
         soup = BeautifulSoup(page)
-        for item in soup.find_all("li",{"style":"margin-bottom: 10px"}):
+        for item in soup.find_all("li", {"style": "margin-bottom: 10px"}):
             for link in item.find_all("a"):
                 if ("https:" in link['href']):
                     sites.append(link['href'])
                 else:
                     sites.append("https://www.pharmanity.com" + link['href'])
-    elif ():
-        pass
-
-
-for site in sites:
-    page = urllib.request.urlopen(site).read()
-    soup = BeautifulSoup(page)
-    soup.find("strong",{"itemprop":"name"})
-    break
-
-
-site =sites[0]
-page = urllib.request.urlopen(site).read()
-soup = BeautifulSoup(page)
-namelist =[]
-
-tags = soup.find_all((("h2"),{"class":"ek-bodiv"})).find("div").find("main").find("ul").find_all("li",recursive=False)
-for tag in tags:
-    print(tag.text)
-
-
-tags = soup.find_all("g",{"transform":"scale(0.8605851979345955) translate(13.87999999999998, 0)"})
+        locsites[locat] = {}
+        locsites[locat]["sites"] = sites
 
 
 
-tags = soup.find_all("div",{"class": "col-sm-7"},"<a")
-for i in range(0,len(tags),2):
-    namelist.append(tags[i].text)
+for keyl in list(locsites.keys()):
+    numl = []
+    for site in locsites[keyl]["sites"]:
+        url = sites[len(sites)-1]
+        page_med = urllib2.urlopen(url)
+        soup_med = BeautifulSoup(page_med)
+        item_med = soup_med.find_all("a", class_='menuMedi')[0]
+        link_part_med = item_med['href']
+        link_med = 'https://www.pharmanity.com'+link_part_med
+        page_med_2 = urllib2.urlopen(link_med)
+        soup_med_2 = BeautifulSoup(page_med_2)
+        item_med_2 = soup_med_2.find_all("a", class_='current')[0]
+        txt = str(item_med_2.span.text)
+        meds =int(txt[txt.find("(")+1:txt.find(")")])
+        item_para = soup_med.find_all("a", class_='menuPara')[0]
+        link_part_para = item_para['href']
+        link_para = 'https://www.pharmanity.com'+link_part_para
+        page_para_2 = urllib2.urlopen(link_para)
+        soup_para_2 = BeautifulSoup(page_para_2)
+        item_para_2 = soup_para_2.find("div", class_='sidebox menuparapharmacie')
+        strong_para_2 = item_para_2.find_all("strong")
+        pharms = 0
+        for strong in strong_para_2:
+            no = str(strong.span.text)
+            number =int(no[no.find("(")+1:no.find(")")])
+            pharms = pharms + number
 
-    print(tag.text)
-addlist = []
-for i in range(1, len(tags), 2):
-    addlist.append(tags[i].text)
+        final_sum = meds + pharms
+        numl.append(final_sum)
+    locsites[locat]["numl"] = numl
+    print(final_sum)
+    print('\n')
 
-tags = soup.find_all("body", {"class": "ek-bodiv"})
 
-from requests_html import HTMLSession
-session = HTMLSession()
-r = session.get('https://python.org/')
 
-children = tags.findChildren("h2", recursive=False)
 
-for tag in tags:
-    print(tag.text)
+
 
 
